829aa526da10ed65acd7c9a91f49609c62b0bacb
diegoarff <diegorinconf1@gmail.com>
2023-06-24 23:13:31 -0400

    feat(client): edit and add some components

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,27 +3,29 @@ import Girl from "../assets/girl.png";
 import Logo from "../assets/PodTalk ogo.png";
 
 export default function Hero() {
-  return (
-    <>
-      <div className={`container ${Style.flex}`}>
-        <section className={Style.colLeft}>
-          <img src={Logo} />
-          <h1 className="title">
-            Potencia tu podcast con nuestros servicios especializados
-          </h1>
-          <p>
-            Te ayudamos a llevar tu idea al siguiente nivel para que destaques
-            en el mundo del podcasting
-          </p>
-          <br />
-          <a className={Style.btn} href="#service">
-            Solicitar
-          </a>
-        </section>
-        <section className={Style.colRight}>
-          <img className={Style.image} src={Girl} />
-        </section>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<main
+				className={`container  padding-y ${Style.flex}`}
+				id="bienvenida">
+				<section className={Style.colLeft}>
+					<img className={Style.logo} src={Logo} />
+					<h1 className="title">
+						Potencia tu podcast con nuestros servicios
+						especializados
+					</h1>
+					<p>
+						Te ayudamos a llevar tu idea al siguiente nivel para que
+						destaques en el mundo del podcasting
+					</p>
+					<a className={Style.btn} href="#solicitar">
+						Solicitar
+					</a>
+				</section>
+				<section className={`${Style.colRight} ${Style.mask}`}>
+					<img className={Style.image} src={Girl} />
+				</section>
+			</main>
+		</>
+	);
 }
--- client/src/hooks/useSpotify.js
+++ client/src/hooks/useSpotify.js
@@ -1,37 +1,36 @@
 import { useState, useEffect } from "react";
 
-const SPOTIFY_URL_TOKEN =
-  import.meta.env.VITE_SPOTIFY_URL_TOKEN || process.env.SPOTIFY_URL_TOKEN;
+const SPOTIFY_URL_TOKEN = import.meta.env.VITE_SPOTIFY_URL_TOKEN;
 
 export default function useSpotify() {
-  const [isLogin, setIsLogin] = useState(false);
-  const [error, setError] = useState(null);
+	const [isLogin, setIsLogin] = useState(false);
+	const [error, setError] = useState(null);
 
-  useEffect(() => {
-    if (window.location.search) {
-      const params = new URLSearchParams(window.location.search);
-      const data = {
-        code: params.get("code"),
-        state: params.get("state"),
-      };
+	useEffect(() => {
+		if (window.location.search) {
+			const params = new URLSearchParams(window.location.search);
+			const data = {
+				code: params.get("code"),
+				state: params.get("state"),
+			};
 
-      if (data.state !== window.sessionStorage.getItem("state")) {
-        setError({ error: "state_mismatch" });
-      }
+			if (data.state !== window.sessionStorage.getItem("state")) {
+				setError({ error: "state_mismatch" });
+			}
 
-      window.history.pushState(null, null, "/");
-      const url = SPOTIFY_URL_TOKEN;
-      const options = {
-        method: "POST",
-        body: JSON.stringify(data),
-      };
+			window.history.pushState(null, null, "/");
+			const url = SPOTIFY_URL_TOKEN;
+			const options = {
+				method: "POST",
+				body: JSON.stringify(data),
+			};
 
-      fetch(url, { ...options })
-        .then((res) => res.json())
-        .then(() => setIsLogin(true))
-        .catch((error) => setError(error));
-    }
-  }, []);
+			fetch(url, { ...options })
+				.then((res) => res.json())
+				.then(() => setIsLogin(true))
+				.catch((error) => setError(error));
+		}
+	}, []);
 
-  return { isLogin, error };
+	return { isLogin, error };
 }

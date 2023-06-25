7576ce9f42fa0b74a8d105639893f6b7ffe7f276
Keiber Urbila <keiberup.dev@gmail.com>
2023-06-25 19:56:52 -0400

    refactor(server): email date format changed

--- server/api/sheet/routes.py
+++ server/api/sheet/routes.py
@@ -11,9 +11,9 @@ from utils.validations import validate_json_sheet
 from . import route
 
 SHEET_SECRET = environ.get("SHEET_SECRET")
-SHEET_SUBSCRIPTIONS_URL = environ.get("SHEET_SUBSCRIPTIONS_URL", "")
+SHEET_PODCAST_URL = environ.get("SHEET_PODCAST_URL", "")
 
-DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+DATE_FORMAT = "%Y-%m-%d"
 LANGS_MAP = {
     "es": "%d/%m/%Y",
     "en": "%m/%d/%Y",
@@ -45,7 +45,7 @@ def index(sheet="podcasts"):
     if not sheet in ("subs", "podcasts"):
         abort(404)
 
-    url = SHEET_SUBSCRIPTIONS_URL + "?sheet=" + sheet
+    url = SHEET_PODCAST_URL + "?sheet=" + sheet
     res = requests.get(url, headers=header)
     return res.json()
 
@@ -71,9 +71,9 @@ def add_sub():
     dt_object = datetime.strptime(body["appointment"], DATE_FORMAT)
 
     header = {"Authorization": f"Bearer {SHEET_SECRET}"}
-    body["appointment"] = dt_object.strftime("%d/%m/%Y %H:%M:%S")
-    body["subscription"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    res = requests.post(SHEET_SUBSCRIPTIONS_URL, headers=header, json=body)
+    body["appointment"] = dt_object.strftime("%d/%m/%Y")
+    body["subscription"] = datetime.now().strftime("%d/%m/%Y")
+    res = requests.post(SHEET_PODCAST_URL, headers=header, json=body)
 
     # Create a Message object with the appointment details and send it using Flask-Mail
     sms = Message("Cita Podscat-T2 agendada", recipients=[body["email"]])

eb3385b10e0fc9e68755e02ca73280647d1a6398
Keiber Urbila <keiberup.dev@gmail.com>
2023-06-25 12:00:04 -0400

    style(Spotify): 🎨 added animations to iframes added an animation to the hover that zooms and increases the contrast a bit

--- client/src/components/Spotify/Spotify.jsx
+++ client/src/components/Spotify/Spotify.jsx
@@ -5,34 +5,34 @@ import Style from "./Spotify.module.css";
 import useSpotify from "../../hooks/useSpotify";
 
 export default function Spotify() {
-	const data = useSpotify();
+  const data = useSpotify();
 
-	if (data.error) alert(JSON.stringify(data));
+  if (data.error) alert(JSON.stringify(data));
 
-	return (
-		<>
-			<div className={`container padding-y ${Style.flex}`} id="spotify">
-				<section className={Style.colLeft}>
-					<h3 className="title">
-						¿Quieres publicar tu podcast en Spotify y llegar a una
-						audiencia global?
-					</h3>
-					<p>
-						Somos una plataforma fácil de usar que te permite
-						publicar tu podcast en Spotify y llegar a más de 450
-						millones de oyentes en más de 200 países y regiones
-					</p>
-					<p>
-						Además, si necesitas más información sobre cómo publicar
-						tu podcast en Spotify, te podemos ayudar.
-					</p>
-				</section>
+  return (
+    <>
+      <div className={`container padding-y ${Style.flex}`} id="spotify">
+        <section className={Style.colLeft}>
+          <h3 className="title">
+            ¿Quieres publicar tu podcast en Spotify y llegar a una audiencia
+            global?
+          </h3>
+          <p>
+            Somos una plataforma fácil de usar que te permite publicar tu
+            podcast en Spotify y llegar a más de 450 millones de oyentes en más
+            de 200 países y regiones
+          </p>
+          <p>
+            Además, si necesitas más información sobre cómo publicar tu podcast
+            en Spotify, te podemos ayudar.
+          </p>
+        </section>
 
-				<section className={Style.colRight}>
-					{!data.access_token ? <Login /> : <Logged />}
-				</section>
-			</div>
-			<div className={Style.bgDark}>{data.access_token && <Podcasts />}</div>
-		</>
-	);
+        <section className={Style.colRight}>
+          {!data.access_token ? <Login /> : <Logged />}
+        </section>
+      </div>
+      <div className={Style.bgDark}>{data.access_token && <Podcasts />}</div>
+    </>
+  );
 }

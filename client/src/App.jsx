53134fefaeba9c4d1681bda81bb6b4c8283492c5
diegoarff <diegorinconf1@gmail.com>
2023-07-04 20:41:20 -0400

    refactor(Testimonials): move Testimonial to a new file and better practices

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,23 @@ import { SheetContextProvider } from "./context/SheetContext";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Spotify from "./components/Spotify/Spotify";
-import TestimonialSlider from "./components/TestimonialSlider";
+import TestimonialSlider from "./components/Testimonials/TestimonialSlider";
 import ApplyForm from "./components/ApplyForm/ApplyForm";
 import Footer from "./components/Footer";
 
 export default function App() {
-  return (
-    <>
-    <SheetContextProvider>
-      <Navbar />
-        <main>
-          <Hero />
-          <Spotify />
-          <TestimonialSlider />
-          <ApplyForm />
-        </main>
-      </SheetContextProvider>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<SheetContextProvider>
+				<Navbar />
+				<main>
+					<Hero />
+					<Spotify />
+					<TestimonialSlider />
+					<ApplyForm />
+				</main>
+			</SheetContextProvider>
+			<Footer />
+		</>
+	);
 }
---
+++ client/src/components/Testimonials/Testimonial.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import Style from "./TestimonialSlider.module.css";
+
+export default function Testimonial({ name, picture, title, message }) {
+	return (
+		<div className={Style.stretch}>
+			<div className={Style.mask}>
+				<img className={Style.image} src={picture} />
+			</div>
+			<div className={`card ${Style.testimonial} ${Style.flex}`}>
+				<div>
+					<h3 className="title">{name}</h3>
+					<p className={`textGradient ${Style.title}`}>{title}</p>
+				</div>
+				<p>{message}</p>
+			</div>
+		</div>
+	);
+}

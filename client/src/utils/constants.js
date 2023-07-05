b000b778955af2dc4b917d94c4597543b5d12c72
diegoarff <diegorinconf1@gmail.com>
2023-07-04 21:19:33 -0400

    feat(client): add constants file to utils

---
+++ client/src/utils/constants.js
@@ -0,0 +1,148 @@
+export default {
+	navbarLinks: ["#bienvenida", "#spotify", "#testimonials", "#solicitar"],
+	es: {
+		navbar: {
+			welcome: "Bienvenida",
+			spotify: "Spotify",
+			testimonials: "Testimonios",
+			request: "Solicitar",
+		},
+		hero: {
+			title: "Potencia tu podcast con nuestros servicios especializados",
+			text: "Te ayudamos a llevar tu idea al siguiente nivel para que destaques en el mundo del podcasting",
+			button: "Solicitar",
+		},
+		spotify: {
+			title: "¿Quieres publicar tu podcast en Spotify y llegar a una audiencia global?",
+			text1: "Somos una plataforma fácil de usar que te permite publicar tu podcast en Spotify y llegar a más de 450 millones de oyentes en más de 100 países y regiones",
+			text2: "Además, si necesitas más información sobre cómo publicar tu podcast en Spotify, te podemos ayudar.",
+			listen: "Escucha nuestros Podcast",
+			login: "Iniciar sesión en Spotify",
+			loggedHeading: "¡Gracias por iniciar sesión!",
+			loggedText: "Baja y descubre nuestro estilo en los Podcast",
+			podcast: "Escucha uno de nuestros podcast",
+		},
+		testimonials: {
+			title: "Testimonios de clientes",
+		},
+		form: {
+			title: "Solicita nuestro servicio",
+			name: "Nombre y Apellido",
+			email: "Correo Electrónico",
+			date: "Fecha",
+			language: "Idioma",
+		},
+		footer: {
+			return: "Volver al inicio",
+		},
+	},
+	en: {
+		navbar: {
+			welcome: "Welcome",
+			spotify: "Spotify",
+			testimonials: "Testimonials",
+			request: "Request",
+		},
+		hero: {
+			title: "Power up your podcast with our specialized services",
+			text: "We help you take your idea to the next level so you can stand out in the world of podcasting",
+			button: "Request",
+		},
+		spotify: {
+			title: "Want to publish your podcast on Spotify and reach a global audience?",
+			text1: "We're an easy-to-use platform that allows you to publish your podcast on Spotify and reach over 450 million listeners in more than 100 countries and regions.",
+			text2: "Plus, if you need more information on how to publish your podcast on Spotify, we can help.",
+			listen: "Listen to our Podcasts",
+			login: "Log in to Spotify",
+			loggedHeading: "Thanks for logging in!",
+			loggedText: "Scroll down and discover our style in Podcasts",
+			podcast: "Listen to one of our podcasts",
+		},
+		testimonials: {
+			title: "Customer Testimonials",
+		},
+		form: {
+			title: "Request our service",
+			name: "Name and Last Name",
+			email: "Email",
+			date: "Date",
+			language: "Language",
+		},
+		footer: {
+			return: "Return to home",
+		},
+	},
+	fr: {
+		navbar: {
+			welcome: "Bienvenue",
+			spotify: "Spotify",
+			testimonials: "Témoignages",
+			request: "Demande",
+		},
+		hero: {
+			title: "Boostez votre podcast avec nos services spécialisés",
+			text: "Nous vous aidons à concrétiser votre idée pour que vous puissiez vous démarquer dans le monde des podcasts.",
+			button: "Demande",
+		},
+		spotify: {
+			title: "Vous voulez publier votre podcast sur Spotify et toucher un public mondial?",
+			text1: "Nous sommes une plateforme facile à utiliser qui vous permet de publier votre podcast sur Spotify et d'atteindre plus de 450 millions d'auditeurs dans plus de 200 pays et régions.",
+			text2: "De plus, si vous avez besoin de plus d'informations sur la publication de votre podcast sur Spotify, nous pouvons vous aider.",
+			listen: "Écoutez nos podcasts",
+			login: "Connexion à Spotify",
+			loggedHeading: "Merci de vous être connecté!",
+			loggedText: "Faites défiler et découvrez notre style de podcasts",
+			podcast: "Écoutez un de nos podcasts",
+		},
+		testimonials: {
+			title: "Témoignages de clients",
+		},
+		form: {
+			title: "Demandez notre service",
+			name: "Nom et Prénom",
+			email: "Email",
+			date: "Date",
+			language: "Langue",
+		},
+		footer: {
+			return: "Retour à l'accueil",
+		},
+	},
+	de: {
+		navbar: {
+			welcome: "Willkommen",
+			spotify: "Spotify",
+			testimonials: "Kundenbewertungen",
+			request: "Anfrage",
+		},
+		hero: {
+			title: "Verleihen Sie Ihrem Podcast mit unseren spezialisierten Dienstleistungen mehr Power",
+			text: "Wir helfen Ihnen, Ihre Idee auf die nächste Stufe zu bringen, damit Sie in der Welt des Podcasting hervorstechen können.",
+			button: "Anfrage",
+		},
+		spotify: {
+			title: "Möchten Sie Ihren Podcast auf Spotify veröffentlichen und ein globales Publikum erreichen?",
+			text1: "Wir sind eine benutzerfreundliche Plattform, mit der Sie Ihren Podcast auf Spotify veröffentlichen und über 450 Millionen Hörer in mehr als 200 Ländern und Regionen erreichen können.",
+			text2: "Wenn Sie weitere Informationen zur Veröffentlichung Ihres Podcasts auf Spotify benötigen, helfen wir Ihnen gerne weiter.",
+			listen: "Hören Sie sich unsere Podcasts an",
+			login: "Bei Spotify anmelden",
+			loggedHeading: "Vielen Dank fürs Einloggen!",
+			loggedText:
+				"Scrollen Sie nach unten und entdecken Sie unseren Stil in Podcasts.",
+			podcast: "Hören Sie sich einen unserer Podcasts an",
+		},
+		testimonials: {
+			title: "Kundenbewertungen",
+		},
+		form: {
+			title: "Fordern Sie unseren Service an",
+			name: "Name und Nachname",
+			email: "E-Mail",
+			date: "Datum",
+			language: "Sprache",
+		},
+		footer: {
+			return: "Zurück zur Startseite",
+		},
+	},
+};
